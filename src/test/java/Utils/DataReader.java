cd6fd07bf1d41c6d5327a2a9d16324af5a573646
alijabee90 <alijabee90@gmail.com>
2024-02-12 02:52:39 -0600

    commiting the homework

---
+++ src/test/java/Utils/DataReader.java
@@ -0,0 +1,39 @@
+package Utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DataReader {
+public class TestDataReader {
+		
+		private static Properties property;
+			
+			static {
+			
+				try {
+					File file = new File("./src/test/resources/testdata/env.properties");
+					FileInputStream input = new FileInputStream(file);
+					property = new Properties();
+					property.load(input);
+					input.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("File Not Found");
+				}
+				
+			}
+			
+			public static  String getProperty(String key) {
+				return property.getProperty(key);
+			}
+}
+
+	public static String getProperty(String string) {
+		
+		return null;
+	}
+
+	}
+
+
---
+++ src/test/java/pages/EbayDemo.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import Utils.Driver;
+
+public class EbayDemo {
+	public static void main(String[] args) {
+		Driver.getDriver()
+}
+
+}
